74cb4cb1b4a67b7418250117a85d17e18b2c830c
Michael Cheng <michael.cheng@flatironschool.com>
2018-08-14 23:20:03 -0400

    removed useless commented code

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,11 +1,6 @@
 import React from 'react';
 
 const Header = () => {
-  // return (
-  //   <header>
-  //     <h1>Flatiron Task Lister</h1>
-  //   </header>
-  // );
   return (
     <div className="nav-bar">
       <ul>
